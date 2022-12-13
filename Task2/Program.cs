c3a03a4915f52c6c0681ffade5d396b5426d895c
Никита К <fox643@yandex.ru>
2022-12-13 16:23:24 +0300

    второй коммит

---
+++ Task2/Program.cs
@@ -0,0 +1,31 @@
+﻿//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+//A (3,6,8); B (2,1,-7), -> 15.84
+//A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.Clear();
+
+int []  AddArray(int[] array)
+{
+     for(int count = 0; count < array.Length; count++)
+    {
+        Console.Write($"Введите {count+1}-ю ось координаты: ");
+        int ver = int.Parse(Console.ReadLine());
+        array[count] = ver;
+    }
+
+    return array;
+}
+
+void FromFirstToSecond(int[] arr1, int [] arr2)
+{
+    double size = Math.Sqrt(Math.Pow(arr2[0]-arr1[0],2)+Math.Pow(arr2[1]-arr1[1],2)+Math.Pow(arr2[2]-arr1[2],2));
+    Console.WriteLine(size);
+    
+}
+
+int [] firstPoint = new int [3];
+firstPoint = AddArray(firstPoint);
+int [] secondPoint = new int [3];
+secondPoint = AddArray(secondPoint);
+
+FromFirstToSecond(firstPoint,secondPoint);
